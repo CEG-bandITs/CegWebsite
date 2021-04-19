c7724b160327bd37385ff3b48b344f49f084c8e4
Muthu Palaniyappan OL <muthu892542@gmail.com>
2021-04-19 16:32:38 +0530

    Commented Animation And Added Details.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,6 @@
-setTimeout(function() {
+setTimeout(function() { 
     var OpeningLogoDiv = document.getElementById("OpeningLogoDiv");
-    OpeningLogoDiv.parentNode.removeChild(OpeningLogoDiv);
-    document.getElementById("MainPageContents").style.display = "block";
-}, 2000);
+    OpeningLogoDiv.parentNode.removeChild(OpeningLogoDiv); // Removing the logo Div After Home Page animation (That 2 seconds animation)
+
+    document.getElementById("MainPageContents").style.display = "block"; // Showing the content in home page after animation.
+}, 2000); // The code mentioned above this line will run after 2 seconds of logo animation.

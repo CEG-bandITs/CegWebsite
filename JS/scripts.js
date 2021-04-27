a1a065ac8b9a4a60df3b95b8e75cf3e1c6a8b2b3
Muthu Palaniyappan OL <muthu892542@gmail.com>
2021-04-27 11:29:10 +0530

    Side Pannel

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,3 +4,10 @@ setTimeout(function() {
 
     document.getElementById("MainPageContents").style.display = "block"; // Showing the content in home page after animation.
 }, 2000); // The code mentioned above this line will run after 2 seconds of logo animation.
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }

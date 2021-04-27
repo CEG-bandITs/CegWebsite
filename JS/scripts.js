f4d46bf8c4004ce1861c531d1da3b830784fe7eb
Muthu Palaniyappan OL <muthu892542@gmail.com>
2021-04-27 12:53:55 +0530

    Top Navigation

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,12 +4,11 @@ setTimeout(function() {
 
     document.getElementById("MainPageContents").style.display = "block"; // Showing the content in home page after animation.
 }, 2000); // The code mentioned above this line will run after 2 seconds of logo animation.
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("MainPageContents").style.marginLeft = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("MainPageContents").style.marginLeft= "0";
-  }
+
+function openNav(){
+document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
